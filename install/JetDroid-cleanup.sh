e012bc535c6773cb55994a598701358d328dd527
Dopi <dopi711@googlemail.com>
2012-04-18 13:42:16 +0300

    some changes for GB cleanup

--- install/JetDroid-cleanup.sh
+++ install/JetDroid-cleanup.sh
@@ -1,20 +1,28 @@
 #/bin/bash
+  rm -rf acct
   rm -rf cache
   rm -rf config
-  rm d
+  rm -rf d
   rm -rf data
   rm default.prop
   rm -rf dev
-  rm etc
+  rm -rf etc
   rm init
   rm init.goldfish.rc
+  rm init.jet.rc 
   rm init.rc
   #mv init.rc init.rc.bak
+  rm -rf lib
+  rm -rf mnt
   rm -rf proc
+  rm -rf radio 
+  rm recovery.rc 
   rm -rf sbin
   rm -rf sdcard
+  rm -rf sd-ext
   rm -rf sqlite_stmt_journals
   rm -rf sys
   rm -rf system
+  rn ueventd.goldfish.rc ueventd.jet.rc ueventd.rc
+  rm -rf vendor
   rm -rf xbin
-  rm -rf sd-ext
